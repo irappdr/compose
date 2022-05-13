e0244037a585ab3b91d885d4bb999990890f3999
irappdr <89965864+irappdr@users.noreply.github.com>
2022-05-13 09:59:52 +0430

    Add files via upload
    LoadingAnimation

---
+++ LoadingAnimation.kt
@@ -0,0 +1,97 @@
+package com.example.test_compose
+
+
+import android.content.Context
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.TypedValue
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+import kotlin.random.Random
+
+
+class LoadingAnimation : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { run() }
+    }
+}
+
+@Preview
+@Composable
+fun run(){
+
+    LoadingAnimation(Modifier.padding(10.dp))
+}
+
+@Composable
+fun LoadingAnimation(
+    modifier: Modifier = Modifier,
+    circleSize: Dp = 15.dp,
+    circleColor: Color = MaterialTheme.colors.secondary,
+    spaceBetween: Dp = 10.dp,
+    travelDistance: Dp = 7.dp
+) {
+
+
+    val circles = listOf(
+        remember { Animatable(initialValue = 0f) },
+        remember { Animatable(initialValue = 0f) },
+        remember { Animatable(initialValue = 0f) }
+    )
+
+    circles.forEachIndexed { index, animatable ->
+        LaunchedEffect(key1 = animatable) {
+            delay(index * 100L)
+            animatable.animateTo(
+                targetValue = 1f,
+                animationSpec = infiniteRepeatable(
+                    animation = keyframes {
+                        durationMillis = 1200
+                        0.0f at 0 with LinearOutSlowInEasing
+                        1.0f at 300 with LinearOutSlowInEasing
+                        0.0f at 600 with LinearOutSlowInEasing
+                        0.0f at 1200 with LinearOutSlowInEasing
+                    },
+                    repeatMode = RepeatMode.Restart
+                )
+            )
+        }
+    }
+
+    val circleValues = circles.map { it.value }
+    val distance = with(LocalDensity.current) { travelDistance.toPx() }
+    val lastCircle = circleValues.size - 1
+
+    Row(modifier = modifier) {
+        circleValues.forEachIndexed { index, value ->
+            Box(modifier = Modifier
+                .size(circleSize)
+                .graphicsLayer { translationY = -value * distance }
+
+                .background(color = circleColor, shape = CircleShape)
+            )
+            if (index != lastCircle) Spacer(modifier = Modifier.width(spaceBetween))
+        }
+    }
+}
